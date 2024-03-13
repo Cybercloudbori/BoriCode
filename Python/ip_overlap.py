77fff3d7ea0e66da26d57cf38da8a2bc91d494d1
Juan Pagan <jupagan@microsoft.com>
2024-03-13 17:00:31 -0400

    Update IP ranges in check_ip_overlap function

--- Python/ip_overlap.py
+++ Python/ip_overlap.py
@@ -9,7 +9,7 @@ def check_ip_overlap(ip_ranges):
     return False  # No overlap found
 
 # Example usage:
-ip_ranges = ["172.16.29.0/24", "172.16.0.0/25", "172.16.1.0/24", "172.16.2.0/25", "172.16.3.0/24", "172.16.4.0/26", "172.16.4.64/27","172.16.28.0/24"]
+ip_ranges = ["172.16.47.0/24", "172.16.46.0/24", "172.16.32.0/26", "172.16.33.0/24", "172.16.34.0/25"]
 if check_ip_overlap(ip_ranges):
     print("Overlap detected!")
 else:
